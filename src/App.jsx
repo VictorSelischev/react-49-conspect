ff71566499ed2ce8fd94fe6e43ecde1a316d6d36
Victor Selischev <95778687+VictorSelischev@users.noreply.github.com>
2022-09-22 23:18:47 +0300

    Проверка деплоя

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,36 @@
-export default function App() {
-    return <div></div>;
-}
+import { Profile } from './components/Profile/Profile';
+// import { Statistics } from './Statistics/Statistics';
+// import { FriendList } from './FriendList/FriendList';
+// import { TransactionHistory } from './TransactionHistory/TransactionHistory';
+
+import user from './components/Profile/user.json';
+// import data from './Statistics/data.json';
+// import friends from './FriendList/friends.json';
+// import transactions from './TransactionHistory/transactions.json';
+
+export const App = () => {
+  return (
+    <div
+      style={{
+        // height: '100vh',
+        paddingTop: 88,
+        paddingBottom: 88,
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 40,
+        color: '#010101',
+        backgroundColor: '#e7ecf2',
+      }}
+    >
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+    </div>
+  );
+};
