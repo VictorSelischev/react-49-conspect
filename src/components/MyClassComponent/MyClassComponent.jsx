1c37b5015a28abfaca51eae04825f44e72719d84
Victor Selischev <95778687+VictorSelischev@users.noreply.github.com>
2022-09-25 13:08:16 +0300

    Создает компонент класс MyClassComponent

---
+++ src/components/MyClassComponent/MyClassComponent.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class MyClassComponent extends Component {
+  static defaultProps = {};
+
+  static propTypes = {};
+
+  render() {
+    return <div>Class Component</div>;
+  }
+}
+
+export { MyClassComponent };
